136ea9ea5d1f0e3d7f0a811f2e35f29534f06694
blackingbar28 <blackingbar28@gmail.com>
2017-11-08 21:57:40 +0700

    add layout

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -2,18 +2,18 @@
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>@yield('title')</title>
+        @yield('title')
         <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
         <!-- Bootstrap 3.3.2 -->
         <link href="{!! asset('/css/bootstrap.min.css') !!}" rel="stylesheet" type="text/css" />
         <link href="{!! asset('/css/font-awesome.min.css') !!}" rel="stylesheet" type="text/css" />
         <link href="{!! asset('/css/bulma.css') !!}" rel="stylesheet" type="text/css" />
+        <link href="{!! asset('/css/style.css') !!}" rel="stylesheet" type="text/css" />
         <!-- Font Awesome Icons -->
         <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
         @yield('styles')
     </head>
 <body>
-
     @yield('content')
     <!-- jQuery 2.1.3 -->
     <script src="{!! asset ('/js/jquery-3.2.1.min.js') !!}"></script>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,78 +1,116 @@
 @extends('layouts.master')
-
+@section('title')
+<title>livestream bundesliga - fußball bundesliga live stream | Hdfootball.de</title>
+<meta name='description' content='alle bundesliga spiele anschauen on Hdfootball.de for free' />
+<meta name="keywords" content='fußball bundesliga live, fussball live tv, live tv fussball, fußball live im tv, fußball heute im tv, fussball im tv, bundesliga livestream net, fußball heute, fußball live heute, fußball heute live, fußball bundesliga heute, fußball heute abend, fußball live stream kostenlos heute, fußball heute live stream, champions league live stream kostenlos heute, fußball live gucken,live übertragung fußball heute, live streaming fußball, bundesliga stream ru'/>
+@endsection
 @section('content')
 
-    <div class="site-nav">
-        <nav class="navbar is-primary">
-            <div class="navbar-brand">
-                <a class="navbar-item" href="https://bulma.io">
+<div class="site-nav">
+    <nav class="navbar is-primary">
+        <div class="navbar-brand">
+            <a class="navbar-item" href="https://bulma.io">
 
-                    <img src="https://bulma.io/images/bulma-logo-white.png" alt="Bulma: a modern CSS framework based on Flexbox" width="112" height="28">
-
-                </a>
-                <div class="navbar-burger burger" data-target="navMenuColorprimary-example">
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                </div>
+                <img src="https://bulma.io/images/bulma-logo-white.png" alt="Bulma: a modern CSS framework based on Flexbox" width="112" height="28">
+            </a>
+            <div class="navbar-burger burger" data-target="navMenuColorprimary-example">
+                <span></span>
+                <span></span>
+                <span></span>
             </div>
-            <!-- nav content -->
-            <div id="navMenuColorprimary-example" class="navbar-menu">
-                <div class="navbar-start">
-                    <a class="navbar-item" href="https://bulma.io/">
-                        Home
-                    </a>
-                    <a class="navbar-item" href="https://bulma.io/">
-                        Home
-                    </a>
-                    <a class="navbar-item" href="https://bulma.io/">
-                        Home
-                    </a>
-                    <a class="navbar-item" href="https://bulma.io/">
-                        Home
-                    </a>
-                    <a class="navbar-item" href="https://bulma.io/">
-                        Home
-                    </a>
-                </div>
+        </div>
+        <!-- nav content -->
+        <div id="navMenuColorprimary-example" class="navbar-menu">
+            <div class="navbar-start">
+                <a class="navbar-item" href="https://bulma.io/">
+                    Home
+                </a>
+                <a class="navbar-item" href="https://bulma.io/">
+                    Home
+                </a>
+                <a class="navbar-item" href="https://bulma.io/">
+                    Home
+                </a>
+                <a class="navbar-item" href="https://bulma.io/">
+                    Home
+                </a>
+                <a class="navbar-item" href="https://bulma.io/">
+                    Home
+                </a>
+            </div>
 
-                <div class="navbar-end">
-                    <div class="navbar-item">
-                        <div class="field is-grouped">
-                            <p class="control">
-                                <a class="bd-tw-button button" data-social-network="Twitter" data-social-action="tweet" data-social-target="http://localhost:4000" target="_blank" href="https://twitter.com/intent/tweet?text=Bulma: a modern CSS framework based on Flexbox&amp;hashtags=bulmaio&amp;url=http://localhost:4000&amp;via=jgthms">
-                                    <span class="icon">
-                                        <i class="fa fa-twitter"></i>
-                                    </span>
-                                    <span>
-                                        Tweet
-                                    </span>
-                                </a>
-                            </p>
-                            <p class="control">
-                                <a class="button is-primary" href="https://github.com/jgthms/bulma/archive/0.5.1.zip">
-                                      <span class="icon">
-                                        <i class="fa fa-download"></i>
-                                      </span>
-                                    <span>Download</span>
-                                </a>
-                            </p>
-                        </div>
+            <div class="navbar-end">
+                <div class="navbar-item">
+                    <div class="field is-grouped">
+                        <p class="control">
+                            <a class="bd-tw-button button" data-social-network="Twitter" data-social-action="tweet" data-social-target="http://localhost:4000" target="_blank" href="https://twitter.com/intent/tweet?text=Bulma: a modern CSS framework based on Flexbox&amp;hashtags=bulmaio&amp;url=http://localhost:4000&amp;via=jgthms">
+                                <span class="icon">
+                                    <i class="fa fa-twitter"></i>
+                                </span>
+                                <span>
+                                    Tweet
+                                </span>
+                            </a>
+                        </p>
+                        <p class="control">
+                            <a class="button is-primary" href="https://github.com/jgthms/bulma/archive/0.5.1.zip">
+                                  <span class="icon">
+                                    <i class="fa fa-download"></i>
+                                  </span>
+                                <span>Download</span>
+                            </a>
+                        </p>
                     </div>
                 </div>
             </div>
-            <!-- end nav content -->
+        </div>
+        <!-- end nav content -->
+    </nav>
+</div>
+
+<div class="container content-wrap">
+    <div class="site-hd">
+        <nav class="breadcrumb" aria-label="breadcrumbs">
+            <ul>
+                <li><a href="#"><span class="icon is-small"><i class="fa fa-home"></i></span><span>Bulma</span></a></li>
+                <li><a href="#"><span class="icon is-small"><i class="fa fa-book"></i></span><span>Documentation</span></a></li>
+                <li><a href="#"><span class="icon is-small"><i class="fa fa-puzzle-piece"></i></span><span>Components</span></a></li>
+                <li class="is-active"><a href="#" aria-current="page"><span class="icon is-small"><i class="fa fa-thumbs-up" aria-hidden="true"></i></span><span>Breadcrumb</span></a></li>
+            </ul>
         </nav>
     </div>
-
-    <section class="section">
-        <div class="wrap-content">
-            <h1 class="title">Section</h1>
-            <h2 class="subtitle">
-                A simple container to divide your page into <strong>sections</strong>, like the one you're currently reading
-            </h2>
+    <div class="site-content">
+        <div class="time">
+            <p>07.11.17</p>
         </div>
-    </section>
+        <div class="wrap-match">
+            <div class="row match-content">
+                <div class="col-xs-3 match-time">
+                    <p>16:00 - 18:00</p>
+                </div>
+                <div class="col-xs-7 match-link">
+                    <p> CR Belouizdad - Oran </p>
+                </div>
+                <div class="col-xs-2 match-status">
+                    <p>Live</p>
+                </div>
+            </div>
+            <div class="row match-content">
+                <div class="col-xs-3 match-time">
+                    <p>16:00 - 18:00</p>
+                </div>
+                <div class="col-xs-7 match-link">
+                    <p> CR Belouizdad - Oran </p>
+                </div>
+                <div class="col-xs-2 match-status">
+                    <p>Live</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="site-footer"></div>
+</div>
+
 
     <script>
         document.addEventListener('DOMContentLoaded', function () {
